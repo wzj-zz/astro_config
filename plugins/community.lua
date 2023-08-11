ec4522f94d92ab9bc7f9ba9c9540c6b6922ae44c
wzj-zz <wzj_reverse@outlook.com>
2023-08-12 07:09:48 +0800

    feature: astro config 0.0.13

--- plugins/community.lua
+++ plugins/community.lua
@@ -22,7 +22,25 @@ return {
     },
   },
 
+  { import = "astrocommunity.project.nvim-spectre" },
+  {
+    "nvim-pack/nvim-spectre",
+    cmd = "Spectre",
+    keys = function()
+      return {
+        { "<leader>sp", function() require("spectre").toggle() end, desc = "Toggle Spectre" },
+        {
+          "<leader>sw",
+          mode = "x",
+          function() require("spectre").open_visual { select_word = true } end,
+          desc = "Spectre (current word)",
+        },
+        { "<leader>sf", function() require("spectre").open_file_search() end, desc = "Spectre (current file)" },
+      }
+    end,
+  },
+
   { import = "astrocommunity.debugging.nvim-bqf" },
 
-  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.motion.mini-surround" },
 }
