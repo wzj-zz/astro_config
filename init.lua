11712dd59eeb77f743e83efd051d7872ef67ada1
wzj-zz <wzj_reverse@outlook.com>
2023-08-12 03:44:24 +0800

    feature: astro config 0.0.11

--- init.lua
+++ init.lua
@@ -64,28 +64,4 @@ return {
       },
     },
   },
-
-  -- This function is run last and is a good place to configuring
-  -- augroups/autocommands and custom filetypes also this just pure lua so
-  -- anything that doesn't fit in the normal config locations above can go here
-  polish = function()
-    -- Set up custom filetypes
-    -- vim.filetype.add {
-    --   extension = {
-    --     foo = "fooscript",
-    --   },
-    --   filename = {
-    --     ["Foofile"] = "fooscript",
-    --   },
-    --   pattern = {
-    --     ["~/%.config/foo/.*"] = "fooscript",
-    --   },
-    -- }
-
-    vim.api.nvim_create_autocmd("User", {
-      desc = "Set number in Telescope",
-      pattern = "TelescopePreviewerLoaded",
-      command = "setlocal number",
-    })
-  end,
 }
