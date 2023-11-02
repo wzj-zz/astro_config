acb0d97f4afb00ba7ab44bd6962c6d6979eefc10
wzj-zz <wzj_reverse@outlook.com>
2023-11-02 20:06:02 +0800

    feature: astro config 0.0.38

--- plugins/comm-pack.lua
+++ plugins/comm-pack.lua
@@ -2,6 +2,18 @@ return {
   "AstroNvim/astrocommunity",
 
   { import = "astrocommunity.pack.cpp" },
+
+  {
+    "Civitasv/cmake-tools.nvim",
+    lazy = false,
+    opts = function(_, opts)
+      local out_path = "out/${variant:buildType}"
+      if vim.fn.has "win64" == 1 then out_path = string.gsub(out_path, "/", "\\") end
+      opts.cmake_build_directory = out_path
+      return opts
+    end,
+  },
+
   { import = "astrocommunity.pack.java" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python" },
