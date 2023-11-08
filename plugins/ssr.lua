bd9589a7fe08b17bbb237ce19e913f8279dbcd31
wzj-zz <wzj_reverse@outlook.com>
2023-11-09 07:33:31 +0800

    feature: astro config 0.0.49

---
+++ plugins/ssr.lua
@@ -0,0 +1,25 @@
+return {
+  "cshuaimin/ssr.nvim",
+  -- Calling setup is optional.
+  config = function()
+    require("ssr").setup {
+      border = "rounded",
+      min_width = 50,
+      min_height = 5,
+      max_width = 120,
+      max_height = 25,
+      adjust_window = true,
+      keymaps = {
+        close = "q",
+        next_match = "n",
+        prev_match = "N",
+        replace_confirm = "<cr>",
+        replace_all = "<leader><cr>",
+      },
+    }
+  end,
+
+  keys = {
+    { "<leader>sr", mode = { "n", "x" }, function() require("ssr").open() end, desc = "Structural Search and Replace" },
+  },
+}
